6360d1d17204fee4b69d34b1b4c75fc880af5ba3
Duubz <duubz@outlook.com>
2014-09-29 05:10:28 -0500

    Update URLProc.py

--- URLProc.py
+++ URLProc.py
@@ -3,23 +3,21 @@ import urllib,urllib2,json,cookielib,time
 import SSL_Wrap
 
 class URLProc:
-	def __init__(self,url,protocol,queue):
+	def __init__(self,url,protocol):
 		self.comprehension = dict()
 		self.url           = url
 		self.protocol      = protocol
 		self.headers       = dict()
-		self.cookies       = dict()
 		self.cookie_handle = cookielib.LWPCookieJar()
 		self.hanlders      = [urllib2.HTTPHandler(), urllib2.HTTPSHandler(), urllib2.HTTPCookieProcessor(self.cookie_handle)]
 		self.management    = urllib2.build_opener(*self.hanlders)
-		self.queue         = queue
 		self.ssl           = dict()
+		self.cookies       = dict()
 
 	def Retrieve(self,url,protocol,type):
 		if type == 0:
 			if protocol == 0: request = urllib2.Request("http://%s"%(url))
 			if protocol == 1: request = urllib2.Request("https://%s"%(url))
-
 			self.management.open(request)
 			for cookie in self.cookie_handle:
 				self.cookies[cookie.name] = json.dumps(cookie.value)
@@ -27,7 +25,6 @@ class URLProc:
 		if type == 1:
 			if protocol == 0: request = "http://%s"%(url)
 			if protocol == 1: request = "https://%s"%(url)
-
 			for header,value in urllib2.urlopen(request).headers.items():
 				self.headers[header] = json.dumps(value)
 			return self.headers
@@ -43,11 +40,5 @@ class URLProc:
 			return self.ssl
 
 	def Feed(self):
-		if(self.queue != None)and(len(self.queue)>0):
-			for url in self.queue:
-				tmp = {"protocol":url[1], "cookies":self.Retrieve(url[0],url[1],0), "headers":self.Retrieve(url[0],url[1],1), "ssl":self.Retrieve(url[0],url[1],2) }
-				self.comprehension[url[0]] = tmp
-		else:
-			tmp = {"protocol":self.protocol, "cookies":self.Retrieve(self.url,self.protocol,0), "headers":self.Retrieve(self.url,self.protocol,1), "ssl":self.Retrieve(self.url,self.protocol,2)}
-			self.comprehension[self.url] = tmp
-		return self.comprehension
+		tmp = {"protocol":self.protocol, "cookies":self.Retrieve(self.url,self.protocol,0), "headers":self.Retrieve(self.url,self.protocol,1), "ssl":self.Retrieve(self.url,self.protocol,2)}
+		return tmp
