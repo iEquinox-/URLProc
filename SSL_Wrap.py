9b7dbc3290d0695323ecf299a9599a92c374066f
Duubz <duubz@outlook.com>
2014-09-29 04:26:53 -0500

    Create SSL_Wrap.py
    Adds SSL support

---
+++ SSL_Wrap.py
@@ -0,0 +1,35 @@
+import socket,ssl
+
+class Wrap:
+	def __init__(self, url):
+
+		if not url.split(".")[0] == "www":
+			self.url    = "www." + url
+		else:
+			self.url    = url
+
+		self.exceptions = ["cloudflare.com"]
+		self.socket     = socket.socket()
+		self.ssl_file   = "/home/equinox/Desktop/sslkeys.pem"
+
+	def Attempt(self):
+		try:
+			c = ssl.wrap_socket(
+				self.socket,
+				cert_reqs   = ssl.CERT_REQUIRED,
+				ssl_version = ssl.PROTOCOL_SSLv3,
+				ca_certs    = self.ssl_file
+				)
+			c.connect( ( self.url , 443 ) )
+			certificate = c.getpeercert()
+			commonName  = certificate['subject'][4][0][1]
+			if commonName != self.url:
+				if not ".".join(commonName.split(".")[1:]) in self.exceptions:
+					return [False,"Unverified"]
+				else:
+					return [True,"Verified"]
+			else:
+				return [True,"Verified"]
+		except Exception as e:
+			print "Error parsed; " + str(e)
+			quit()
