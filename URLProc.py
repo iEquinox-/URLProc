f6b4e0609c186d56cc916e36da0f5d0cf00bbbb0
Duubz <duubz@outlook.com>
2014-09-29 04:26:30 -0500

    Update URLProc.py
    Added SSL support

--- URLProc.py
+++ URLProc.py
@@ -1,4 +1,6 @@
-import urllib,urllib2,json,cookielib
+import urllib,urllib2,json,cookielib,time
+
+import SSL_Wrap
 
 class URLProc:
 	def __init__(self,url,protocol,queue):
@@ -11,6 +13,7 @@ class URLProc:
 		self.hanlders      = [urllib2.HTTPHandler(), urllib2.HTTPSHandler(), urllib2.HTTPCookieProcessor(self.cookie_handle)]
 		self.management    = urllib2.build_opener(*self.hanlders)
 		self.queue         = queue
+		self.ssl           = dict()
 
 	def Retrieve(self,url,protocol,type):
 		if type == 0:
@@ -28,13 +31,23 @@ class URLProc:
 			for header,value in urllib2.urlopen(request).headers.items():
 				self.headers[header] = json.dumps(value)
 			return self.headers
+		if type == 2:
+			if protocol == 0:
+				self.ssl["SSL"] = False
+			if protocol == 1:
+				wrapped = SSL_Wrap.Wrap(url=url).Attempt()
+				if wrapped[0] == True:
+					self.ssl["SSL"] = json.dumps(wrapped[1])
+				else:
+					self.ssl["SSL"] = json.dumps(wrapped[0])
+			return self.ssl
 
 	def Feed(self):
 		if(self.queue != None)and(len(self.queue)>0):
 			for url in self.queue:
-				tmp = {"protocol":url[1], "cookies":self.Retrieve(url[0],url[1],0), "headers":self.Retrieve(url[0],url[1],1) }
+				tmp = {"protocol":url[1], "cookies":self.Retrieve(url[0],url[1],0), "headers":self.Retrieve(url[0],url[1],1), "ssl":self.Retrieve(url[0],url[1],2) }
 				self.comprehension[url[0]] = tmp
 		else:
-			tmp = {"protocol":self.protocol, "cookies":self.Retrieve(self.url,self.protocol,0), "headers":self.Retrieve(self.url,self.protocol,1)}
+			tmp = {"protocol":self.protocol, "cookies":self.Retrieve(self.url,self.protocol,0), "headers":self.Retrieve(self.url,self.protocol,1), "ssl":self.Retrieve(self.url,self.protocol,2)}
 			self.comprehension[self.url] = tmp
 		return self.comprehension
